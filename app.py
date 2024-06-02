50d7a81a99e2bdab857ac55f850272e79c56c555
Dheeman Das <84588268+Reliable-Royalty-29@users.noreply.github.com>
2024-06-02 19:08:53 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,72 +1,106 @@
-#Conversational Q&A Chatbot
-import os
-from dotenv import load_dotenv
-from langchain_openai import AzureOpenAI
 import streamlit as st
-from langchain.schema import HumanMessage,SystemMessage,AIMessage
+from PyPDF2 import PdfReader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+import os
+from langchain_google_genai import GoogleGenerativeAIEmbeddings
+import google.generativeai as genai
+from langchain.vectorstores import FAISS
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain.chains.question_answering import load_qa_chain
+from langchain.prompts import PromptTemplate
+from dotenv import load_dotenv
 
-# Load environment variables from .env file
 load_dotenv()
-
-# # Define classes for different types of messages
-# class SystemMessage:
-#     def __init__(self, content):
-#         self.content = content
-
-# class HumanMessage:
-#     def __init__(self, content):
-#         self.content = content
-
-# class AIMessage:
-#     def __init__(self, content):
-#         self.content = content
-
-OPENAI_API_VERSION = "2024-02-01"
-azure_openai_api_key = os.environ.get('AZURE_OPENAI_API_KEY')
-
-llm = AzureOpenAI(
-    deployment_name="Dheeman",
-    api_version=OPENAI_API_VERSION,
-    azure_endpoint="https://d2912.openai.azure.com/",
-    temperature=0.8
-   
-)
-
-#Streamlit UI
-
-# st.set_page_config(page_title="Conversational Q&A Chatbot")
-# st.header("Hey, lets chat with AI")
+os.getenv("GOOGLE_API_KEY")
+genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
 
 
-if 'msg' not in st.session_state:
-  st.session_state['msg']=[
-    SystemMessage(content="Answer anything that huamna ask")
-  ]
 
-def test(question):
-    st.session_state['msg'].append(HumanMessage(content=question))
+
+
+
+def get_pdf_text(pdf_docs):
+    text=""
+    for pdf in pdf_docs:
+        pdf_reader= PdfReader(pdf)
+        for page in pdf_reader.pages:
+            text+= page.extract_text()
+    return  text
+
+
+
+def get_text_chunks(text):
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=10000, chunk_overlap=1000)
+    chunks = text_splitter.split_text(text)
+    return chunks
+
+
+def get_vector_store(text_chunks):
+    embeddings = GoogleGenerativeAIEmbeddings(model = "models/embedding-001")
+    vector_store = FAISS.from_texts(text_chunks, embedding=embeddings)
+    vector_store.save_local("faiss_index")
+
+
+def get_conversational_chain():
+
+    prompt_template = """
+    Answer the question as detailed as possible from the provided context, make sure to provide all the details, if the answer is not in
+    provided context just say, "answer is not available in the context", don't provide the wrong answer\n\n
+    Context:\n {context}?\n
+    Question: \n{question}\n
+
+    Answer:
+    """
+
+    model = ChatGoogleGenerativeAI(model="gemini-pro",
+                             temperature=0.3)
+
+    prompt = PromptTemplate(template = prompt_template, input_variables = ["context", "question"])
+    chain = load_qa_chain(model, chain_type="stuff", prompt=prompt)
+
+    return chain
+
+
+
+def user_input(user_question):
+    embeddings = GoogleGenerativeAIEmbeddings(model = "models/embedding-001")
     
-    # Concatenate the content of messages into a single prompt string
-    prompt = "\n".join(msg.content for msg in st.session_state['msg'])
+    new_db = FAISS.load_local("faiss_index", embeddings)
+    docs = new_db.similarity_search(user_question)
+
+    chain = get_conversational_chain()
+
     
-    # Pass the concatenated prompt string to the language model
-    answer = llm(prompt)
-    
-    st.session_state['msg'].append(AIMessage(content=answer))
-    return answer
+    response = chain(
+        {"input_documents":docs, "question": user_question}
+        , return_only_outputs=True)
+
+    print(response)
+    st.write("Reply: ", response["output_text"])
 
 
-st.set_page_config(page_title="Q&A Demo")
 
-st.header("Friendly Chatbot Application")
 
-input=st.text_input("Ask your questions: ",key="input")
-response=test(input)
+def main():
+    st.set_page_config("Chat PDF")
+    st.header("Chat with PDF using Gemini💁")
 
-submit=st.button("Ask the question")
+    user_question = st.text_input("Ask a Question from the PDF Files")
 
-## If ask button is clicked
+    if user_question:
+        user_input(user_question)
 
-if submit:
-    st.subheader("The Response is")
-    st.write(response)
+    with st.sidebar:
+        st.title("Menu:")
+        pdf_docs = st.file_uploader("Upload your PDF Files and Click on the Submit & Process Button", accept_multiple_files=True)
+        if st.button("Submit & Process"):
+            with st.spinner("Processing..."):
+                raw_text = get_pdf_text(pdf_docs)
+                text_chunks = get_text_chunks(raw_text)
+                get_vector_store(text_chunks)
+                st.success("Done")
+
+
+
+if __name__ == "__main__":
+    main()
